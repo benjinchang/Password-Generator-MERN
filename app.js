583f93310159fb7be99ac1ec8a94a0d92d6bb7f1
benjinchang <benjinchang@gmail.com>
2017-06-26 02:36:18 -0700

    deployment

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var generatePassword = require('password-generator');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +17,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/public')));
-
-app.use('/', index);
-app.use('/users', users);
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.set('view engine', 'jade');
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +39,27 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+  var count = 5;
+
+  // Generate some passwords
+  var passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var generatePassword = require('password-generator');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-	// Comment out this line:
-  //res.send('respond with a resource');
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+// 	// Comment out this line:
+//   //res.send('respond with a resource');
 
-  // And insert something like this instead:
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }, {
-    id: 3,
-    username: "Testing1"
-  }
-  ]);
+//   // And insert something like this instead:
+//   res.json([{
+//   	id: 1,
+//   	username: "samsepi0l"
+//   }, {
+//   	id: 2,
+//   	username: "D0loresH4ze"
+//   }, {
+//     id: 3,
+//     username: "Testing1"
+//   }
+//   ]);
+// });
+// Put all API endpoints under '/api'
+router.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
 });
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+});
+
 
 module.exports = router;
